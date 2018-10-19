2c7bbe41568bc63badeb9ba7292a3811c20adb0e
Jock Lawrie <jock.lawrie@gmail.com>
2018-10-19 11:53:52 +1100

    Tweaks.

--- src/BucketStores.jl
+++ src/BucketStores.jl
@@ -1,7 +1,13 @@
+"""
+Defines behaviour of bucket stores (concrete subtypes of AbstractBucketStore).
+
+Also defines fields that are common to all bucket stores.
+"""
 module BucketStores
 
 
-export BucketStore, AbstractBackend,                      # Types
+export AbstractBucketStore,
+       @add_BucketStore_common_fields,                    # Used in constructor of concrete subtypes
        listcontents, createbucket!, deletebucket!,        # Buckets
        getindex, setindex!, delete!,                      # Objects
        islocal, isbucket, hasbucket, isobject, hasobject  # Conveniences
@@ -9,7 +15,86 @@ export BucketStore, AbstractBackend,                      # Types
 
 import Base.setindex!, Base.getindex, Base.delete!
 
-abstract type AbstractBackend end
+abstract type AbstractBucketStore end
+
+const type2module = Dict{DataType, Module}()  # Enables concrete types to access behaviour of AbstractBucketStore
+
+
+################################################################################
+# Constructor
+
+"""
+Adds fields that are common to all concrete subtypes of AbstractBucketStore.
+
+Use this macro when defining a concrete subtype of AbstractBucketStore.
+
+For example, the LocalDiskStore is defined as follows (see the LocalDiskStores.jl package):
+
+  struct LocalDiskStore <: AbstractBucketStore
+      @add_BucketStore_common_fields  # Fields that all bucket stores require
+  end
+
+You can also add fields that are specific to your type.
+For example:
+
+  struct MyStore <: AbstractBucketStore
+      @add_BucketStore_common_fields  # Fields that all bucket stores require
+      otherfield1::Int                # Field that is specific to MyStore
+      otherfield2::String             # Field that is specific to MyStore
+  end
+"""
+macro add_BucketStore_common_fields()
+    return esc(:(
+        permission::Symbol;                # One of :readonly, :limited, :unlimited
+        root::String;                      # Name of root bucket, which contains all buckets and objects in the store.
+        names::Dict{String, Set{String}};  # bucketname => Set(bucket_names..., object_names...). bucketname excludes root. Names exclude root/bucketname.
+        bucketnames::Set{String};)         # Names of buckets created by the store (not all buckets in names were created by the store).
+    )
+end
+
+
+"""
+Constructor for concrete subtypes.
+
+Signature is T(permission, root, type_specific_args...)
+
+Examples:
+- From the LocalDiskStores package:  LocalDiskStore(:limited, "/tmp/rootbucket")
+"""
+function (::Type{T})(permission::Symbol, root::String, type_specific_args...) where {T <: AbstractBucketStore}
+    !(T <: AbstractBucketStore) && error("Store is not a subtype of AbstractBucketStore")
+    !in(permission, Set([:readonly, :limited, :unlimited])) && error("Permission must be one of :readonly, :limited, :unlimited")
+    type2module[T] = parentmodule(T)  # Register the package that defines T
+    newstore = T(permission, root, Dict{String, Set{String}}(root => Set{String}()), Set{String}(), type_specific_args...)
+    isobject(newstore, root) && error("Root already exists as an object. Cannot use it as a bucket.")
+    if !isbucket(newstore, root)  # Root does not exist...create it
+        if permission == :readonly
+            error("Root bucket does not exist and could not be created because permission is read-only.")
+        else
+            ok = createbucket!(newstore, root)
+            if !ok
+                error("Root bucket does not exist and could not be created.")
+            end
+        end
+    end
+    newstore
+end
+
+function isobject(store::T, name::String) where {T <: AbstractBucketStore}
+    m = type2module[typeof(store)]
+    m.isobject(store, joinpath(store.root, name))
+end
+
+function isbucket(store::T, name::String) where {T <: AbstractBucketStore}
+    m = type2module[typeof(store)]
+    m.isbucket(store, joinpath(store.root, name))
+end
+
+function createbucket!(store::T, bktname::String) where {T <: AbstractBucketStore}
+    m = type2module[typeof(store)]
+    m.createbucket!(store, bktname)
+end
+
 
 ################################################################################
 # Constructors
