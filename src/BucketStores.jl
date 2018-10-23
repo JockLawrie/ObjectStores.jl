9ba68f046b88fbc1a1b563f840e932d0121d696f
Jock Lawrie <jock.lawrie@gmail.com>
2018-10-23 13:29:25 +1100

    Improved the README. Renamed the repo.

--- src/BucketStores.jl
+++ src/BucketStores.jl
@@ -1,20 +1,36 @@
-module AbstractBucketStores
+module BucketStores
 
 export BucketStore, AbstractStorageBackend,        # Types
        createbucket!, listcontents, deletebucket!, # Buckets: create/update, read, delete
        setindex!, getindex, delete!,               # Objects: create/update, read, delete
-       islocal, isbucket, isobject                 # Conveniences
+       islocal, isbucket, isobject,                # Conveniences
+       Permission,                                 # Re-exported from Authorization
+       getpermission, setpermission!, setexpiry!,  # Re-exported from Authorization
+       haspermission, permissions_conflict,        # Re-exported from Authorization
+       @add_required_fields_backend  # Used when constructing concrete subtypes of AbstractStorageBackend
+
 
 using Authorization
 using Logging
 
+
+# Methods that are extended in this package
 import Base.setindex!, Base.getindex, Base.delete!
+import Authorization.setpermission!
 
 
 ################################################################################
 # Types 
 
-abstract type AbstractStorageBackend end  # Required fields: bucket_type, object_type
+abstract type AbstractStorageBackend end
+
+macro add_required_fields_backend()
+    return esc(:(
+                 bucket_type::DataType;
+                 object_type::DataType;
+                ))
+end
+
 
 struct BucketStore{T <: AbstractStorageBackend} <: AbstractClient
     root::String  # ID of root bucket
@@ -26,11 +42,18 @@ struct BucketStore{T <: AbstractStorageBackend} <: AbstractClient
         m._isobject(root) && error("Root already exists as an object. Cannot use it as a bucket.")
         newstore = new{typeof(backend)}(root, backend, id, id2permission, idpattern2permission, type2permission)
         if !m._isbucket(root)  # Root does not exist...create it
-            ok, msg = createbucket!(newstore, "")  # 2nd arg "" implies bucketname is root
-            !ok && error(msg)
+            msg = createbucket!(newstore, "")  # 2nd arg "" implies bucketname is root
+            msg != nothing && @warn msg
         end
         newstore
     end
+end
+
+function BucketStore(id, root, backend)
+    id2permission        = Dict{String, Permission}()
+    idpattern2permission = Dict{Regex,  Permission}()
+    type2permission      = Dict{DataType, Permission}()
+    BucketStore(id, root, backend, id2permission, idpattern2permission, type2permission)
 end
 
 
@@ -39,21 +62,17 @@ end
 
 "Create bucket. If successful return nothing, else return an error message as a String."
 function createbucket!(store::BucketStore, bucketname::String="")
-    backend = store.backend
-    B = backend.bucket_type
-    m = parentmodule(typeof(backend))
+    B = store.backend.bucket_type
     resourceid = bucketname == "" ? store.root : joinpath(store.root, bucketname)
-    create!(store, m.B(resourceid))
+    create!(store, B(resourceid))
 end
 
 
-"Read bucket. If successful return the value, else @warn the error message and return nothing."
+"List the contents of the bucket. If successful return the value, else @warn the error message and return nothing."
 function listcontents(store::BucketStore, bucketname::String="")
-    backend = store.backend
-    B = backend.bucket_type
-    m = parentmodule(typeof(backend))
+    B = store.backend.bucket_type
     resourceid = bucketname == "" ? store.root : joinpath(store.root, bucketname)
-    ok, val = read(store, m.B(resourceid))
+    ok, val = read(store, B(resourceid))
     if !ok
         @warn val
         return nothing
@@ -64,11 +83,9 @@ end
 
 "Delete bucket. If successful return nothing, else return an error message as a String."
 function deletebucket!(store::BucketStore, bucketname::String)
-    backend = store.backend
-    B = backend.bucket_type
-    m = parentmodule(typeof(backend))
+    B = store.backend.bucket_type
     resourceid = bucketname == "" ? store.root : joinpath(store.root, bucketname)
-    delete!(store, m.B(resourceid))
+    delete!(store, B(resourceid))
 end
 
 
@@ -84,16 +101,14 @@ function setindex!(store::BucketStore, v, i::String)
     cb, shortname = splitdir(fullpath)
     !m._isbucket(cb) && return (false, "Cannot create object $(i) inside a non-existent bucket.")
     O = backend.object_type
-    create!(store, m.O(fullpath), v)
+    create!(store, O(fullpath), v)
 end
 
 
 "Read object. If successful return the value, else @warn the error message and return nothing."
 function getindex(store::BucketStore, i::String)
-    backend = store.backend
-    O = backend.object_type
-    m = parentmodule(typeof(backend))
-    ok, val = read(store, m.O(joinpath(store.root, i)))
+    O = store.backend.object_type
+    ok, val = read(store, O(joinpath(store.root, i)))
     if !ok
         @warn val
         return nothing
@@ -104,10 +119,8 @@ end
 
 "Delete object. If successful return nothing, else return an error message as a String."
 function delete!(store::BucketStore, i::String)
-    backend = store.backend
-    O = backend.object_type
-    m = parentmodule(typeof(backend))
-    delete!(store, m.O(joinpath(store.root, i)))
+    O = store.backend.object_type
+    delete!(store, O(joinpath(store.root, i)))
 end
 
 
@@ -131,5 +144,12 @@ function isobject(store::BucketStore, name::String)
     m = parentmodule(typeof(store.backend))
     m._isobject(joinpath(store.root, name))
 end
+
+function setpermission!(store::BucketStore, resourcetype::Symbol, p::Permission)
+    resourcetype == :bucket && return setpermission!(store, store.backend.bucket_type, p)
+    resourcetype == :object && return setpermission!(store, store.backend.object_type, p)
+    @warn "Resource type unknown. Permission not set."
+end
+
 
 end
